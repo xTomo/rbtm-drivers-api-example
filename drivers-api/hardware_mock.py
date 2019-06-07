358e58ee78ff6a8d857fc5e5fe65aa398d7a767d
gogbajbobo <grigoriev.maxim@gmail.com>
2019-06-07 18:07:22 +0300

    set motor states

--- drivers-api/hardware_mock.py
+++ drivers-api/hardware_mock.py
@@ -1,19 +1,32 @@
 def get_motor_state(motor_id):
 
-    state = motor_state(motor_id)
+    state = None
+
+    if motor_id == 0:
+        state = 'I am a vertical motor'
+    elif motor_id == 1:
+        state = 'I am a horizontal motor'
+    elif motor_id == 2:
+        state = 'I am an angle motor'
+
     error_message = None if state else 'There is no motor with id {}'.format(motor_id)
     result = {'motor_id': motor_id, 'state': state} if state else None
 
     return result, error_message
 
 
-def motor_state(motor_id):
+def set_motor_state(motor_id, new_state):
+
+    state = None
 
     if motor_id == 0:
-        return 'I am a vertical motor'
+        state = 'Vertical motor state is updated to {}'.format(new_state)
     elif motor_id == 1:
-        return 'I am a horizontal motor'
+        state = 'Horizontal motor state is updated to {}'.format(new_state)
     elif motor_id == 2:
-        return 'I am an angle motor'
-    else:
-        return None
+        state = 'Angular motor state is updated to {}'.format(new_state)
+
+    error_message = None if state else 'There is no motor with id {}'.format(motor_id)
+    result = {'motor_id': motor_id, 'state': state} if state else None
+
+    return result, error_message
--- drivers-api/routes.py
+++ drivers-api/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, request
 import json
 from . import hardware_mock as hw
 
@@ -15,24 +15,65 @@ def main_route():
 
 
 # Motor routes
+@bp_motor.after_request
+def after_request(response):
+    header = response.headers
+    header['Content-Type'] = 'application/json; charset=utf-8'
+    return response
+
+
 @bp_motor.route('/', methods=['GET'])
 def get_state(motor_id):
 
-    motor_state, error_message = hw.get_motor_state(motor_id)
-    success = True if error_message is None else False
+    motor_state, error = hw.get_motor_state(motor_id)
+    success = error is None
 
     return create_response(
         success=success,
         result=motor_state,
-        error_message=error_message
+        error=error
+    )
+
+
+@bp_motor.route('/', methods=['POST'])
+def set_state(motor_id):
+
+    json_data, error = check_request(request.data)
+    success = error is None
+
+    if not success:
+        return create_response(
+            success=success,
+            error=error
+        )
+
+    motor_state, error = hw.set_motor_state(motor_id, json_data)
+
+    return create_response(
+        success=error is None,
+        result=motor_state,
+        error=error
     )
 
 
 # functions
-def create_response(success=True, error_message=None, result=None):
+def create_response(success=True, error=None, result=None):
     response_dict = {
         'success': success,
-        'error': error_message,
+        'error': error,
         'result': result,
     }
     return json.dumps(response_dict)
+
+
+def check_request(request_data):
+
+    if not request_data:
+        return None, create_response(success=False, error='Request is empty')
+
+    try:
+        request_data_dict = json.loads(request_data)
+    except TypeError:
+        return None, create_response(success=False, error='Request has not JSON data')
+    else:
+        return request_data_dict, None
